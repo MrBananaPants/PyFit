85f4f519ef2944eaf477a7106e38098e581c1dda
MrBananaPants <joran.vancoillie@gmail.com>
2022-06-19 21:21:49 +0200

    Changed name of default exercise

--- PyFit.py
+++ PyFit.py
@@ -14,10 +14,10 @@ def check_files():
         os.makedirs(path)
         print("DIRECTORY CREATED")
     # Check if the needed files exist. If not, create them
-    workout_file = Path(os.path.join(path, "exercise.json"))
+    workout_file = Path(os.path.join(path, "default.json"))
     workout_file.touch(exist_ok=True)
-    if os.path.getsize(os.path.join(path, "exercise.json")) == 0:
-        file = open(os.path.join(path, "exercise.json"), "a")
+    if os.path.getsize(os.path.join(path, "default.json")) == 0:
+        file = open(os.path.join(path, "default.json"), "a")
         file.write(
             '{ "exercises": [ { "name": "Push-ups", "reps": 10, "sets": 5 }, { "name": "Leg raises", "reps": 30, "sets": 1 }, { "name": "Hip raises", "reps": 30, "sets": 1 }, { "name": "Toe touches", "reps": 30, "sets": 1 }, { "name": "Flutter kicks", "reps": 30, "sets": 1 }, { "name": "Sit-ups", "reps": 30, "sets": 1 }, { "name": "Pull-ups", "reps": 10, "sets": 1 }, { "name": "Chin-ups", "reps": 10, "sets": 1 }, { "name": "Biceps", "reps": 10, "sets": 1 }, { "name": "Forward fly", "reps": 10, "sets": 1 }, { "name": "Side fly", "reps": 10, "sets": 1 }, { "name": "Forearms", "reps": 50, "sets": 2 } ] }')
         file.close()
@@ -32,7 +32,7 @@ def resetWorkoutView():
 def viewWorkoutButtonEvent():
     resetWorkoutView()
     print("VIEW WORKOUT")
-    file = open(os.path.join(path, "exercise.json"), "r")
+    file = open(os.path.join(path, "default.json"), "r")
     data = file.read()
     source = json.loads(data)
     exercises = source["exercises"]
@@ -67,7 +67,7 @@ def nextStep():
     global exerciseSet
     global totalRepCount
     global showRestScreen
-    file = open(os.path.join(path, "exercise.json"), "r")
+    file = open(os.path.join(path, "default.json"), "r")
     data = file.read()
     source = json.loads(data)
     exercises = source["exercises"]

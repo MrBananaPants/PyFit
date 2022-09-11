302a34f467e446f8eb3b6326b9f7d5e395c75c77
MrBananaPants <joran.vancoillie@gmail.com>
2022-09-11 17:21:56 +0200

    Added media directory to data files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Usage:
 from setuptools import setup
 
 APP = ['PyFit.py']
-DATA_FILES = []
+DATA_FILES = [('', ['media'])]
 OPTIONS = {
     'iconfile': 'media/icon.icns',
     'plist': {'CFBundleShortVersionString': '0.5.1', },

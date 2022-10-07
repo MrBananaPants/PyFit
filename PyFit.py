b7055af2e8e4fbddb002d9881a49609f8ab9e135
MrBananaPants <joran.vancoillie@gmail.com>
2022-10-07 14:33:40 +0200

    Bump version

--- PyFit.py
+++ PyFit.py
@@ -22,7 +22,7 @@ else:
     main_path = os.path.join(os.getenv("HOME"), "PyFit")
 
 # Global variables
-version = "0.6.1"
+version = "0.6.2"
 exercise_index = 0
 info_index = 0
 exercise_list = []
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ APP = ['PyFit.py']
 DATA_FILES = [('', ['media'])]
 OPTIONS = {
     'iconfile': 'media/icon.icns',
-    'plist': {'CFBundleShortVersionString': '0.6.1', },
+    'plist': {'CFBundleShortVersionString': '0.6.2', },
     'packages': ['customtkinter', 'PIL']}
 
 setup(
